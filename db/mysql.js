61cabb1534d24681002c72f00d19b004b614428d
Nitin Nithilesh <nitin@yellowmessenger.com>
2021-06-11 22:30:48 +0530

    Changes in mysql conn

--- db/mysql.js
+++ db/mysql.js
@@ -15,29 +15,29 @@ if (config.APP_ENV === 'sandbox') {
     function connect() {
         console.log('[MYSQL] Connected...');
 
-        conn = mysql.createConnection({
+        conn = mysql.createPool({
             host: config.MYSQL_PROD.HOST,
             user: config.MYSQL_PROD.USER,
             password: config.MYSQL_PROD.PASSWORD,
             database: config.MYSQL_PROD.DB
         })
 
-        conn.connect(function (err) {
-            if (err) {
-                console.log('[MYSQL] Error: ', err);
-                console.log('[MYSQL] Trying to connect after 2 sec...');
-                setTimeout(connect, 2000);
-            }
-        });
+        // conn.connect(function (err) {
+        //     if (err) {
+        //         console.log('[MYSQL] Error: ', err);
+        //         console.log('[MYSQL] Trying to connect after 2 sec...');
+        //         setTimeout(connect, 2000);
+        //     }
+        // });
 
-        conn.on('error', function (err) {
-            if (err.code == 'PROTOCOL_CONNECTION_LOST') {
-                console.log('[MYSQL] Host trying to disconnect with code PROTOCOL_CONNECTION_LOST. Reconnecting...');
-                connect();
-            } else {
-                console.log('[MYSQL] Error: ', err);
-            }
-        });
+        // conn.on('error', function (err) {
+        //     if (err.code == 'PROTOCOL_CONNECTION_LOST') {
+        //         console.log('[MYSQL] Host trying to disconnect with code PROTOCOL_CONNECTION_LOST. Reconnecting...');
+        //         connect();
+        //     } else {
+        //         console.log('[MYSQL] Error: ', err);
+        //     }
+        // });
     }
 
     connect();
