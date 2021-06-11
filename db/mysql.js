10cd52d9f109a05825fe1cd909d825136326ab13
Nitin Nithilesh <nitin@yellowmessenger.com>
2021-06-11 22:15:23 +0530

    Mysql changes
    Mysql host changed to prod
    Changed the connection setup of mysql

--- db/mysql.js
+++ db/mysql.js
@@ -3,12 +3,45 @@
 const mysql = require('mysql');
 const config = require('../config');
 
-var conn = mysql.createConnection({
-    host: config.MYSQL.HOST,
-    user: config.MYSQL.USER,
-    password: config.MYSQL.PASSWORD,
-    database: config.MYSQL.DB
-});
+let conn;
+if (config.APP_ENV === 'sandbox') {
+    conn = mysql.createConnection({
+        host: config.MYSQL_SANDBOX.HOST,
+        user: config.MYSQL_SANDBOX.USER,
+        password: config.MYSQL_SANDBOX.PASSWORD,
+        database: config.MYSQL_SANDBOX.DB
+    });
+} else {
+    function connect() {
+        console.log('[MYSQL] Connected...');
+
+        conn = mysql.createConnection({
+            host: config.MYSQL_PROD.HOST,
+            user: config.MYSQL_PROD.USER,
+            password: config.MYSQL_PROD.PASSWORD,
+            database: config.MYSQL_PROD.DB
+        })
+
+        conn.connect(function (err) {
+            if (err) {
+                console.log('[MYSQL] Error: ', err);
+                console.log('[MYSQL] Trying to connect after 2 sec...');
+                setTimeout(connect, 2000);
+            }
+        });
+
+        conn.on('error', function (err) {
+            if (err.code == 'PROTOCOL_CONNECTION_LOST') {
+                console.log('[MYSQL] Host trying to disconnect with code PROTOCOL_CONNECTION_LOST. Reconnecting...');
+                connect();
+            } else {
+                throw err;
+            }
+        });
+    }
+
+    connect();
+}
 
 function exeQuery(query) {
     return new Promise((resolve, reject) => {
