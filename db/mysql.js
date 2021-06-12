8d07edfe60676f512bef10fa5edf1d5679401ae9
Nitin Nithilesh <nitin@yellowmessenger.com>
2021-06-12 17:09:21 +0530

    UI changes

--- db/mysql.js
+++ db/mysql.js
@@ -12,35 +12,14 @@ if (config.APP_ENV === 'sandbox') {
         database: config.MYSQL_SANDBOX.DB
     });
 } else {
-    function connect() {
-        console.log('[MYSQL] Connected...');
+    console.log('[MYSQL] Connected...');
 
-        conn = mysql.createPool({
-            host: config.MYSQL_PROD.HOST,
-            user: config.MYSQL_PROD.USER,
-            password: config.MYSQL_PROD.PASSWORD,
-            database: config.MYSQL_PROD.DB
-        })
-
-        // conn.connect(function (err) {
-        //     if (err) {
-        //         console.log('[MYSQL] Error: ', err);
-        //         console.log('[MYSQL] Trying to connect after 2 sec...');
-        //         setTimeout(connect, 2000);
-        //     }
-        // });
-
-        // conn.on('error', function (err) {
-        //     if (err.code == 'PROTOCOL_CONNECTION_LOST') {
-        //         console.log('[MYSQL] Host trying to disconnect with code PROTOCOL_CONNECTION_LOST. Reconnecting...');
-        //         connect();
-        //     } else {
-        //         console.log('[MYSQL] Error: ', err);
-        //     }
-        // });
-    }
-
-    connect();
+    conn = mysql.createPool({
+        host: config.MYSQL_PROD.HOST,
+        user: config.MYSQL_PROD.USER,
+        password: config.MYSQL_PROD.PASSWORD,
+        database: config.MYSQL_PROD.DB
+    });
 }
 
 function exeQuery(query) {
