4661592fbddc460b5c0b466ae07cb356bda628b5
Nitin Nithilesh <nitin@yellowmessenger.com>
2021-06-18 08:56:24 +0530

    Added column in db

--- models/feed.js
+++ models/feed.js
@@ -4,7 +4,7 @@ let Mysql = require('../db/mysql');
 
 function tweet(tweet, feeling) {
     let date = new Date();
-    let query = `INSERT INTO feed VALUES ('${tweet}', '${feeling}', '${date.toGMTString()}')`;
+    let query = `INSERT INTO feed (tweet, feeling, time) VALUES ('${tweet}', '${feeling}', '${date.toGMTString()}')`;
 
     let result = Mysql.exeQuery(query);
 
@@ -12,7 +12,7 @@ function tweet(tweet, feeling) {
 }
 
 function getTweets() {
-    let query = 'SELECT * FROM feed ORDER BY time DESC';
+    let query = 'SELECT * FROM feed ORDER BY id DESC';
 
     let result = Mysql.exeQuery(query);
 
